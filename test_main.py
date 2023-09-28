7d07f1ec01f21c283d63a38bcdfa981b0f2c5999
hxia5 <hxia5@illinois.edu>
2023-09-28 03:17:26 -0400

    Test

--- test_main.py
+++ test_main.py
@@ -98,16 +98,19 @@ def test_query1():
 def test_query2():
     query2(c)
 
-# if __name__ == "__main__":
-#     test_extract()
-#     test_load()
-#     c, conn = test_connect()
-#     test_create()
-#     test_insert()
-#     test_read()
-#     test_update_Shape_Leng()
-#     test_delete()
-#     test_query1()
-#     test_query2()
-#     conn.close()
-#     print("All passed!")
+def test_main():
+    test_extract()
+    test_load()
+    c, conn = test_connect()
+    test_create()
+    test_insert()
+    test_read()
+    test_update_Shape_Leng()
+    test_delete()
+    test_query1()
+    test_query2()
+    conn.close()
+    print("All passed!")
+
+if __name__ == "__main__":
+    test_main()
